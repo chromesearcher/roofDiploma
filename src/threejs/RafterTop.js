0c9475a792f3bf459723112a952ed76dd5cea229
chromesearcher <d-kei@yandex.ru>
2018-08-24 14:55:14 +0300

    basic rafters done

---
+++ src/threejs/RafterTop.js
@@ -0,0 +1,23 @@
+
+export default (scene, y) => {    
+
+    var THREE = require('three');   
+
+    var boxGeometry = new THREE.BoxGeometry(0.8, 0.8, 20);
+    boxGeometry.rotateY( Math.PI / 2 );
+    var boxMaterial = new THREE.MeshBasicMaterial({color: 0x1f3a3d, wireframe: false, side: THREE.DoubleSide});
+    var box = new THREE.Mesh(boxGeometry, boxMaterial);
+    box.position.x = 0;
+    box.position.y = y;
+    box.position.z = 0;
+    scene.add(box);
+
+    function update(time) {
+        
+        //box.rotation.x += 0.1
+    }
+
+    return {
+        update
+    }
+}
